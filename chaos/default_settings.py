f4d7f09f60db0f701504a8adca17af4fd12ecb82
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2014-06-04 08:44:32 +0200

    Init chaos

---
+++ chaos/default_settings.py
@@ -0,0 +1,52 @@
+#URI for postgresql
+# postgresql://<user>:<password>@<host>:<port>/<dbname>
+#http://docs.sqlalchemy.org/en/rel_0_9/dialects/postgresql.html#psycopg2
+SQLALCHEMY_DATABASE_URI = 'postgresql://navitia:navitia@localhost/chaos'
+
+#Log Level available
+# - DEBUG
+# - INFO
+# - WARN
+# - ERROR
+
+# logger configuration
+LOGGER = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters':{
+        'default': {
+            'format': '[%(asctime)s] [%(levelname)5s] [%(process)5s] [%(name)25s] %(message)s',
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+        },
+        'celery':{
+            'level': 'INFO',
+        },
+        'sqlalchemy.engine': {
+            'handlers': ['default'],
+            'level': 'WARN',
+            'propagate': True
+        },
+        'sqlalchemy.pool': {
+            'handlers': ['default'],
+            'level': 'WARN',
+            'propagate': True
+        },
+        'sqlalchemy.dialects.postgresql': {
+            'handlers': ['default'],
+            'level': 'WARN',
+            'propagate': True
+        },
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+Chaos
+======
+
+Chaos is the web service who implements the realtime aspect of Navitia
