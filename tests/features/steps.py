1a094dd108c8e8b1045e41290369eeb4db63a939
kadhikari <krishna.adhikari@canaltp.fr>
2014-06-25 15:05:39 +0200

    tests: fonctional tests added for disruptions with pager

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -61,3 +61,11 @@ def given_i_have_the_following_disruptions_in_my_database(step):
 def and_the_field_should_have_a_size_of_n(step, fields, size):
     eq_(len(find_field(world.response_json, fields)), int(size))
 
+@step(u'And the field "([^"]*)" should exist')
+def and_the_field_should_exist(step, fields):
+    assert_equals(len(find_field(world.response_json, fields)), 1)
+
+@step(u'And the field "([^"]*)" should be (\d+)')
+def and_in_the_json_the_field_is_set_to(step, fields, value):
+    value = pythonify(value)
+    eq_(int(find_field(world.response_json, fields)), int(value))
