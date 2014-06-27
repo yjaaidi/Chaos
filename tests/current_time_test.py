a12e6f7d2a6f81913108c8a022348509066ec149
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2014-06-27 10:54:47 +0200

    chaos: remove default value for get_current_time function

--- tests/current_time_test.py
+++ tests/current_time_test.py
@@ -4,8 +4,10 @@ from aniso8601 import parse_datetime
 from datetime import datetime
 import chaos
 
+
 class Obj(object):
     pass
+
 
 #current_time exists
 def test_current_time():
@@ -13,15 +15,15 @@ def test_current_time():
         g.current_time = parse_datetime("2014-10-10T13:15:00Z").replace(tzinfo=None)
         eq_(chaos.utils.get_current_time(), g.current_time)
 
+
 #current_time is exists in g and g.current_time None
-def test_now_current_time():
+def test_now_current_time1():
     with chaos.app.app_context():
         g.current_time = None
-        now = datetime.utcnow()
-        eq_(chaos.utils.get_current_time(now), now)
+        eq_(datetime.utcnow() < chaos.utils.get_current_time(), True)
+
 
 #current_time is not exists
-def test_now_current_time():
+def test_now_current_time2():
     with chaos.app.app_context():
-        now = datetime.utcnow()
-        eq_(chaos.utils.get_current_time(now), now)
+        eq_(datetime.utcnow() < chaos.utils.get_current_time(), True)
