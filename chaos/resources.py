6d48a48bbf7decc2145751f7bc57c2cacec9ced9
kadhikari <krishna.adhikari@canaltp.fr>
2014-07-07 11:58:16 +0200

    chaos : disruption_id deleted.

--- chaos/resources.py
+++ chaos/resources.py
@@ -317,11 +317,7 @@ class Impacts(flask_restful.Resource):
         db.session.commit()
         return marshal({'impact': impact}, one_impact_fields), 201
 
-    def put(self, disruption_id, id):
-        if not id_format.match(disruption_id):
-            return marshal({'error': {'message': "id invalid"}},
-                           error_fields), 400
-
+    def put(self, id):
         if not id_format.match(id):
             return marshal({'error': {'message': "id invalid"}},
                            error_fields), 400
