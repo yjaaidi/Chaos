4ead8c124d58e4c97b445387571088bfd713f1d2
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2014-07-09 10:59:20 +0200

    Add channels url on root ressources

--- chaos/resources.py
+++ chaos/resources.py
@@ -86,6 +86,7 @@ class Index(flask_restful.Resource):
             "disruption": {"href": url + '/{id}', "templated": True},
             "severities": {"href": url_for('severity', _external=True)},
             "causes": {"href": url_for('cause', _external=True)},
+            "channels": {"href": url_for('channel', _external=True)},
         }
         return response, 200
 
