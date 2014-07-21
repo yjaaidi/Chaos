76456bfb8b1e70682214ceba46ee554b786c9d3b
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2014-07-21 17:15:14 +0200

    add a setup.py script for compiling .proto files

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Command
+from distutils.spawn import find_executable
+import os
+
+protoc = find_executable("protoc")
+
+class BuildPbfCommand(Command):
+    description = "build protocol buffer files"
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('{protoc} -I. --python_out=chaos *.proto'.format(protoc=protoc))
+
+
+setup(
+        name='chaos',
+        cmdclass={'build_pbf': BuildPbfCommand}
+)
+
