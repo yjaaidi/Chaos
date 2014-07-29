9df1fcaaf8e48a7f2ffdf9bd51d8514ede36171f
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2014-07-29 10:44:26 +0200

    chaos: rename ParseError

--- tests/json_validator_error_test.py
+++ tests/json_validator_error_test.py
@@ -1,5 +1,5 @@
 from nose.tools import *
-from chaos.utils import ParseError
+from chaos.utils import parse_error
 from jsonschema import validate, ValidationError
 from chaos.formats import impact_input_format, channel_input_format, severity_input_format,\
     cause_input_format, disruptions_input_format
@@ -10,7 +10,7 @@ def test_wording_is_required_in_severity():
         validate({}, severity_input_format)
         assert False
     except ValidationError, e:
-        eq_(ParseError(e), "'wording' is a required property", True)
+        eq_(parse_error(e), "'wording' is a required property", True)
 
 
 def test_wording_is_required_in_cause():
@@ -18,7 +18,7 @@ def test_wording_is_required_in_cause():
         validate({}, cause_input_format)
         assert False
     except ValidationError, e:
-        eq_(ParseError(e), "'wording' is a required property", True)
+        eq_(parse_error(e), "'wording' is a required property", True)
 
 
 def test_name_is_required_in_channel():
@@ -26,7 +26,7 @@ def test_name_is_required_in_channel():
         validate({"max_size": 500, "content_type": "text/type"}, channel_input_format)
         assert False
     except ValidationError, e:
-        eq_(ParseError(e), "'name' is a required property", True)
+        eq_(parse_error(e), "'name' is a required property", True)
 
 
 def test_max_size_is_required_in_channel():
@@ -34,7 +34,7 @@ def test_max_size_is_required_in_channel():
         validate({"name": "sms", "content_type": "text/type"}, channel_input_format)
         assert False
     except ValidationError, e:
-        eq_(ParseError(e), "'max_size' is a required property", True)
+        eq_(parse_error(e), "'max_size' is a required property", True)
 
 
 def test_content_type_is_required_in_channel():
@@ -42,7 +42,7 @@ def test_content_type_is_required_in_channel():
         validate({"name": "sms", "max_size": 500}, channel_input_format)
         assert False
     except ValidationError, e:
-        eq_(ParseError(e), "'content_type' is a required property", True)
+        eq_(parse_error(e), "'content_type' is a required property", True)
 
 
 def test_reference_is_required_in_disruption():
@@ -50,7 +50,7 @@ def test_reference_is_required_in_disruption():
         validate({"note": "hello", "cause":{"id": "7ffab230-3d48-4eea-aa2c-22f8680230b6"}}, disruptions_input_format)
         assert False
     except ValidationError, e:
-        eq_(ParseError(e), "'reference' is a required property", True)
+        eq_(parse_error(e), "'reference' is a required property", True)
 
 
 def test_cause_is_required_in_disruption():
@@ -58,7 +58,7 @@ def test_cause_is_required_in_disruption():
         validate({"reference": "foo", "note": "hello"}, disruptions_input_format)
         assert False
     except ValidationError, e:
-        eq_(ParseError(e), "'cause' is a required property", True)
+        eq_(parse_error(e), "'cause' is a required property", True)
 
 
 def test_severity_is_required_in_impcat():
@@ -66,5 +66,5 @@ def test_severity_is_required_in_impcat():
         validate({"application_periods": [{"begin": "2014-04-29T16:52:00Z","end": "2014-05-22T02:15:00Z"},{"begin": "2014-04-29T16:52:00Z","end": "2014-05-22T02:15:00Z"}]}, impact_input_format)
         assert False
     except ValidationError, e:
-        eq_(ParseError(e), "'severity' is a required property", True)
+        eq_(parse_error(e), "'severity' is a required property", True)
 
--- tests/parse_errors_test.py
+++ tests/parse_errors_test.py
@@ -1,5 +1,5 @@
 from nose.tools import *
-from chaos.utils import ParseError
+from chaos.utils import parse_error
 
 import chaos
 
@@ -10,10 +10,10 @@ class Obj(object):
 def test_error_with_message():
     error = Obj()
     error.message = 'message'
-    eq_(ParseError(error), 'message', True)
+    eq_(parse_error(error), 'message', True)
 
 
 def test_error_without_message():
     error = Obj()
     error.message = 'aa \n bbb \n c'
-    eq_(ParseError(error.message), 'aa   bbb   c', True)
+    eq_(parse_error(error.message), 'aa   bbb   c', True)
