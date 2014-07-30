218a626ec287f304a0f5a1cfb3c872102027fa72
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2014-07-30 11:18:26 +0200

    chaos: merge

--- tests/features/terrain.py
+++ tests/features/terrain.py
@@ -10,11 +10,13 @@ migrate = Migrate(chaos.app, chaos.db)
 
 migration_dir = 'migrations' if os.path.isdir('migrations') else '../migrations'
 
+
 @before.each_feature
 def setup_db(feature):
     logging.getLogger('alembic').setLevel(logging.ERROR)
     with chaos.app.app_context():
         flask_migrate.upgrade(directory=migration_dir)
+
 
 @before.each_scenario
 def setup_tester(scenario):
@@ -26,12 +28,14 @@ def teardown_db(feature):
     with chaos.app.app_context():
         flask_migrate.downgrade(revision='base', directory=migration_dir)
 
+
 @after.each_scenario
 def truncate_db(scenario):
     with chaos.app.app_context():
         tables = [str(table) for table in chaos.db.metadata.sorted_tables]
         chaos.db.session.execute('TRUNCATE {} CASCADE;'.format(', '.join(tables)))
         chaos.db.session.commit()
+
 
 @before.each_step
 def retrieve_json_response(step):
