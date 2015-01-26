f9842983f83df5505b090a43f74763e98d636454
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2015-01-26 11:54:31 +0100

    chaos: order category by name

--- chaos/models.py
+++ chaos/models.py
@@ -167,7 +167,7 @@ class Category(TimestampMixin, db.Model):
 
     @classmethod
     def all(cls, client_id):
-        return cls.query.filter_by(client_id=client_id,is_visible=True).all()
+        return cls.query.filter_by(client_id=client_id,is_visible=True).order_by(cls.name).all()
 
     @classmethod
     def get(cls, id, client_id):
