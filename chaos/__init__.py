5a4b47712233b06f60d86f3db726703ab6f16dd8
kadhikari <krishna.adhikari@canaltp.fr>
2015-02-02 09:50:07 +0100

    chaos: livraison de release 0.10.0

--- chaos/__init__.py
+++ chaos/__init__.py
@@ -27,7 +27,7 @@
 # https://groups.google.com/d/forum/navitia
 # www.navitia.io
 
-VERSION = '0.10.0-dev'
+VERSION = '0.10.0'
 
 #remplace blocking method by a non blocking equivalent
 #this enable us to use gevent for launching background task
