a3874fd5c23734042f2a638204ac9cbe1d51a08e
Thierry Marianne <thierry@marianne.io>
2015-12-17 14:28:14 +0100

    Use UTF-8 as database collation at creation

--- provisioning/postgresql/scripts/create-postgresql-user-databases.sh
+++ provisioning/postgresql/scripts/create-postgresql-user-databases.sh
@@ -8,5 +8,11 @@ echo "ALTER ROLE navitia WITH LOGIN;" | psql -U postgres
 
 # Create chaos databases ("chaos" and "chaos_testing") with navitia user as their respective owners
 echo 'Create "chaos" and "chaos_testing" databases with "navitia" as their respective owners'
-createdb -O navitia chaos
-createdb -O navitia chaos_testing
+
+function create_db {
+    createdb -O navitia -E UTF-8 --lc-ctype='en_US.UTF-8' --lc-collate='en_US.UTF-8' -T template0 $1
+}
+
+create_db chaos
+create_db chaos_testing
+
