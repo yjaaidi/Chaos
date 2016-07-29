ac5cd2510bfb79f14ed6640cb52b441427774b9f
tcy <thomas.chevily@canaltp.fr>
2016-07-29 14:56:25 +0200

    Updating Chaos version in master to 0.34.0-dev

--- chaos/__init__.py
+++ chaos/__init__.py
@@ -27,7 +27,7 @@
 # https://groups.google.com/d/forum/navitia
 # www.navitia.io
 
-VERSION = '0.33.0'
+VERSION = '0.34.0-dev'
 
 # remplace blocking method by a non blocking equivalent
 # this enable us to use gevent for launching background task
