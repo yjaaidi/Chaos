7148fd934571f2bccea7c1671a8081af47a73ff3
Vincent Lepot <vincent.lepot@canaltp.fr>
2017-01-11 14:51:56 +0100

    Do not monkey patch threads to be NewRelic-compliant

--- chaos/__init__.py
+++ chaos/__init__.py
@@ -32,7 +32,7 @@ VERSION = '0.35.4-dev'
 # remplace blocking method by a non blocking equivalent
 # this enable us to use gevent for launching background task
 from gevent import monkey
-monkey.patch_all()
+monkey.patch_all(thread=False)
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
