498bd8b2d7a8bb9c7fac16241ab68fe4d6a84d38
tcy <thomas.chevily@canaltp.fr>
2017-02-13 16:29:37 +0100

    Adding cache class

--- chaos/__init__.py
+++ chaos/__init__.py
@@ -40,6 +40,7 @@ import logging.config
 import sys
 from chaos.utils import Request
 from chaos.publisher import Publisher
+from chaos import cache
 
 app = Flask(__name__)
 app.config.from_object('chaos.default_settings')
@@ -58,5 +59,6 @@ else:  # Default is std out
 db = SQLAlchemy(app)
 
 publisher = Publisher(app.config['RABBITMQ_CONNECTION_STRING'], app.config['EXCHANGE'], app.config['ENABLE_RABBITMQ'])
+cache = cache(app, config=app.config['CACHE_CONFIGURATION'])
 
 import chaos.api
