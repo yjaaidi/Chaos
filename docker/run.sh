279c09f067dfb30075fe75a98d8dbeda9887c992
dno <david.negrello@canaltp.fr>
2017-09-15 11:50:09 +0200

    missing step for db

--- docker/run.sh
+++ docker/run.sh
@@ -2,5 +2,5 @@
 
 pip install -r requirements.txt
 ./setup.py build_pbf
-#honcho run ./manage.py db upgrade
+honcho run ./manage.py db upgrade
 exec honcho run ./manage.py runserver --host 0.0.0.0 --port 80
