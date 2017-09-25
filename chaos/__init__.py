059fe2277ff81d9e5bfca7f3a32d46c98657b7b0
dno <david.negrello@canaltp.fr>
2017-09-25 10:33:25 +0200

    up version

--- chaos/__init__.py
+++ chaos/__init__.py
@@ -27,7 +27,7 @@
 # https://groups.google.com/d/forum/navitia
 # www.navitia.io
 
-VERSION = 'v1.0.4-dev'
+VERSION = 'v1.0.4'
 
 # remplace blocking method by a non blocking equivalent
 # this enable us to use gevent for launching background task
