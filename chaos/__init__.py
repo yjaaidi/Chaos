f96fd00d6134ef3e4b03cb7d06f6662952b096c3
Rémy Abi-Khalil <remyabi@gmail.com>
2017-12-13 15:45:34 +0100

    chaos/__init__.py updated for the next new version 1.0.7-dev

--- chaos/__init__.py
+++ chaos/__init__.py
@@ -27,7 +27,7 @@
 # https://groups.google.com/d/forum/navitia
 # www.navitia.io
 
-VERSION = 'v1.0.6'
+VERSION = 'v1.0.7-dev'
 
 # remplace blocking method by a non blocking equivalent
 # this enable us to use gevent for launching background task
