e84ba018dedee3d6dea09fa2594450a520558103
pthegner <pierre.thegner@canaltp.fr>
2019-04-17 13:46:47 +0200

    Fix migration

--- migrations/versions/cf4581a9123_init_history_schema.py
+++ migrations/versions/cf4581a9123_init_history_schema.py
@@ -14,8 +14,6 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
-status_type = sa.Enum('waiting', 'handling', 'error', 'done', name='status')
-
 def upgrade():
     op.execute('CREATE SCHEMA IF NOT EXISTS history')
     op.create_table('disruption',
@@ -27,7 +25,6 @@ def upgrade():
                     sa.Column('note', sa.Text(), nullable=True),
                     sa.Column('start_publication_date', sa.DateTime(), nullable=True),
                     sa.Column('end_publication_date', sa.DateTime(), nullable=True),
-                    sa.Column('localization_id', sa.TEXT(), nullable=True),
                     sa.Column('version', sa.Integer(), nullable=False),
                     sa.Column('client_id', postgresql.UUID(), nullable=False),
                     sa.Column('contributor_id', postgresql.UUID(), nullable=False),
@@ -43,15 +40,14 @@ def upgrade():
                  IF NEW.reference <> OLD.reference OR NEW.note <> OLD.note \
                     OR NEW.start_publication_date <> OLD.start_publication_date \
                     OR NEW.end_publication_date <> OLD.end_publication_date \
-                    OR NEW.localization_id <> OLD.localization_id \
                     OR NEW.client_id <> OLD.client_id OR NEW.contributor_id <> OLD.contributor_id \
                     OR NEW.cause_id <> OLD.cause_id OR NEW.status <> OLD.status \
                     THEN \
                  INSERT INTO history.disruption(created_at,updated_at,disruption_id,reference,note,\
-                    start_publication_date,end_publication_date,localization_id,version,client_id,contributor_id,\
+                    start_publication_date,end_publication_date,version,client_id,contributor_id,\
                     cause_id,status) \
                  VALUES(OLD.created_at,OLD.updated_at,OLD.id,OLD.reference,OLD.note,OLD.start_publication_date,\
-                    OLD.end_publication_date,OLD.localization_id,OLD.version,OLD.client_id,OLD.contributor_id,\
+                    OLD.end_publication_date,OLD.version,OLD.client_id,OLD.contributor_id,\
                     OLD.cause_id,OLD.status); \
                  END IF; \
                  RETURN NEW; \
