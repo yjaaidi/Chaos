938628b40a0fda9cbc9a7797c0abb6a41a34e19a
pthegner <pierre.thegner@canaltp.fr>
2019-04-24 17:13:59 +0200

    Add migration file

---
+++ migrations/versions/d57921a3453_update_carriage_return.py
@@ -0,0 +1,20 @@
+"""Update carriage return on HTML messages
+
+Revision ID: d57921a3453
+Revises: cf4581a9123
+Create Date: 2019-04-24 17:05:02
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'd57921a3453'
+down_revision = 'cf4581a9123'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+def upgrade():
+    op.execute('UPDATE message SET text = regexp_replace(text, E\'[\\\\n\\\\r]\', \'\', \'g\' ) \
+                FROM channel \
+                WHERE message.channel_id = channel.id AND channel.content_type = \'text/html\';')
