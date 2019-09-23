dc7a7703751e9d72beb4e566e690a190ec673237
jenkins <jenkins>
2019-09-23 11:44:42 +0200

    update version 1.2.18-dev to 1.2.18

--- chaos/__init__.py
+++ chaos/__init__.py
@@ -27,7 +27,7 @@
 # https://groups.google.com/d/forum/navitia
 # www.navitia.io
 
-VERSION = 'v1.2.18-dev'
+VERSION = 'v1.2.18'
 
 # remplace blocking method by a non blocking equivalent
 # this enable us to use gevent for launching background task
