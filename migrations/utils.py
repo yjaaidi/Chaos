867c21c7eb4e50ced4358c99b01d810874ccdd53
kun <liangkun2985@gmail.com>
2019-09-23 17:34:05 +0200

    migration correction

---
+++ migrations/utils.py
@@ -0,0 +1,36 @@
+from alembic import op
+
+def index_exists(name):
+    connection = op.get_bind()
+    result = connection.execute(
+        "SELECT exists(SELECT 1 from pg_indexes where indexname = '{}') as ix_exists;"
+            .format(name)
+    ).first()
+    return result.ix_exists
+
+def table_exists(table_name):
+    connection = op.get_bind()
+    result = connection.execute(
+        "SELECT EXISTS( \
+            SELECT * \
+            FROM information_schema.tables \
+            WHERE \
+            table_schema = 'public' AND \
+            table_name = '{}' \
+        ) as ix_exists;".format(table_name)
+            .format(table_name)
+    ).first()
+    return result.ix_exists
+
+def column_exists(table_name, column_name):
+    connection = op.get_bind()
+    result = connection.execute(
+        "SELECT EXISTS ( \
+         SELECT 1  \
+        FROM information_schema.columns \
+        WHERE table_schema='public' \
+        AND table_name='{}' \
+        AND column_name='{}') as ix_exists;".format(table_name, column_name)
+            .format(table_name)
+    ).first()
+    return result.ix_exists
