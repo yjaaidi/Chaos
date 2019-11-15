14938c1578bb4c62401a946def2d167c294907b2
Thomas Chevily <thomas.chevily@canaltp.fr>
2019-11-15 14:15:45 +0100

    Update functional test
    When a disruption is deleted, it couldn't be retrieved in
    disruption/{id} API

--- chaos/models.py
+++ chaos/models.py
@@ -541,13 +541,13 @@ class Disruption(TimestampMixin, db.Model):
             'and_wheres' : [
                 'd.id = :disruption_id',
                 'contributor_id = :contributor_id',
-                'c.is_visible = :cause_is_visisble'
+                'c.is_visible = :cause_is_visible',
                 'd.status <> :excluded_disruption_status'
             ],
             'vars':{
                 'disruption_id': id,
                 'contributor_id': contributor_id,
-                'cause_is_visisble': True,
+                'cause_is_visible': True,
                 'excluded_disruption_status': 'archived'
             },
             'order_by' : {
