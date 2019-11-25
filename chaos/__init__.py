e926e4b7f9d944dd20cdaca056a8d76ae7fd7aa4
jenkins <jenkins>
2019-11-25 11:33:23 +0100

    update version 1.2.23-dev to 1.2.24-dev

--- chaos/__init__.py
+++ chaos/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) since 2001, Kisio Digital and/or its affiliates. All rights reserved.
 
-VERSION = 'v1.2.23-dev'
+VERSION = 'v1.2.24-dev'
 
 # http://bugs.python.org/issue7980
 import datetime
