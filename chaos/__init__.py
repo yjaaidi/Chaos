ce96ef95e10af8a1ac7966ef87af45ad7dc59488
jenkins <jenkins>
2020-03-16 09:49:10 +0100

    update version 1.2.28-dev to 1.2.28

--- chaos/__init__.py
+++ chaos/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) since 2001, Kisio Digital and/or its affiliates. All rights reserved.
 
-VERSION = 'v1.2.28-dev'
+VERSION = 'v1.2.28'
 
 # http://bugs.python.org/issue7980
 import datetime
